1b5c12a249fef1072f897b7ef6173f10ff0091db
Sancho17 <90313185+Sancho17@users.noreply.github.com>
2021-10-13 00:48:26 +1100

    dwadwa
    adw

--- vis.js
+++ vis.js
@@ -8,3 +8,8 @@ var vg_2 = "json/unemployment_bar.json";
 vegaEmbed('#unemployment_bar', vg_2).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
+
+var vg_3 = "json/bubbleplot.json";
+vegaEmbed('#bubbleplot', vg_2).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
