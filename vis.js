1237f647c9425f57c7f6a8744bba18d660c28cb9
Sancho17 <90313185+Sancho17@users.noreply.github.com>
2021-10-13 00:50:06 +1100

    fsefes
    esf

--- vis.js
+++ vis.js
@@ -10,6 +10,6 @@ vegaEmbed('#unemployment_bar', vg_2).then(function(result) {
 }).catch(console.error);
 
 var vg_3 = "json/bubbleplot.json";
-vegaEmbed('#bubbleplot', vg_2).then(function(result) {
+vegaEmbed('#bubbleplot', vg_3).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
