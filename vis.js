8da573530cb14b663d3cb63045b657c3d81a6614
Sancho17 <90313185+Sancho17@users.noreply.github.com>
2021-10-16 19:42:21 +1100

    dawdw
    dwa

--- vis.js
+++ vis.js
@@ -1,20 +1,25 @@
 var vg_1 = "json/map.json";
-vegaEmbed('#map', vg_1).then(function(result) {
+vegaEmbed('#map', vg_1, {"actions" : false}).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
 
 var vg_2 = "json/unemployment.json";
-vegaEmbed('#unemployment', vg_2).then(function(result) {
+vegaEmbed('#unemployment', vg_2, {"actions" : false}).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
 var vg_3 = "json/bubbleplot.json";
-vegaEmbed('#bubbleplot', vg_3).then(function(result) {
+vegaEmbed('#bubbleplot', vg_3, {"actions" : false}).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
 var vg_3 = "json/emissions.json";
-vegaEmbed('#emissions', vg_3).then(function(result) {
+vegaEmbed('#emissions', vg_3, {"actions" : false}).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_4 = "json/internet.json";
+vegaEmbed('#internet', vg_4, {"actions" : false}).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
